90b4abc4e3a8a4d5c7c291077d95edf60c017513
ZoloMohan <mohanzolomon@gmail.com>
2019-07-12 19:22:35 +0530

    Dashboard Booking UI Update, Camp Show Date Init

--- routes/booking.js
+++ routes/booking.js
@@ -8,42 +8,49 @@ router.post("/:id/book", function(req, res){
     Campground.findById(req.params.id, function(error, campground){
         if(error) console.log(error);
         else{
-            Booking.create({
-                campground:{
-                    id: req.params.id,
-                    name: campground.name
-                },
-                hostId: campground.author.id,
-                user:{
-                    id:req.user.id,
-                    username: req.user.username
-                },
-                noOfPeople:{
-                    adults: req.body.adults,
-                    children: req.body.children
-                },
-                noOfNights:req.body.noOfNights,
-                price: (req.body.adults + req.body.children) * campground.price * req.body.noOfNights
-            }, function(error, booking){
-                if(error) console.log(error);
-                else{
-                    User.findById(req.user.id, function(error, user){
-                        if(error) console.log(error);
-                        else{
-                            user.userBookings.push(booking.id);
-                            user.save();
-                            User.findById(campground.author.id, function(error, host){
-                                if(error) console.log(error);
-                                else{
-                                    host.hostedBookings.push(booking.id);
-                                    host.save();
-                                }
-                            })
-                        }
-                    })
-                    res.redirect("/campgrounds/"+req.params.id);
-                }
-            })
+            if(!(campground.author.id == req.user.id)){
+                Booking.create({
+                    campground:{
+                        id: req.params.id,
+                        name: campground.name
+                    },
+                    hostId: campground.author.id,
+                    user:{
+                        id:req.user.id,
+                        username: req.user.username
+                    },
+                    noOfPeople:{
+                        adults: req.body.adults,
+                        children: req.body.children
+                    },
+                    noOfNights:req.body.noOfNights,
+                    price: (req.body.adults + req.body.children) * campground.price * req.body.noOfNights
+                }, function(error, booking){
+                    if(error) console.log(error);
+                    else{
+                        User.findById(req.user.id, function(error, user){
+                            if(error) console.log(error);
+                            else{
+                                user.userBookings.push(booking.id);
+                                user.save();
+                                User.findById(campground.author.id, function(error, host){
+                                    if(error) console.log(error);
+                                    else{
+                                        host.hostedBookings.push(booking.id);
+                                        host.save();
+                                    }
+                                })
+                            }
+                        })
+                        res.redirect("/campgrounds/"+req.params.id);
+                    }
+                })
+            }
+            else{
+                console.log("ou can't book for your own Camp");
+                req.flash("You can't book for your own Camp");
+                res.redirect("/campgrounds/"+req.params.id);
+            }
         }
     })
 })
